0817ff02496f8db20a17ef7d0b3eb106aab44cf9
pxttxrxwxn <naysasatadur5555@gmail.com>
2025-12-18 16:20:04 +0700

    update

--- packages/domain/src/repository/product.ts
+++ packages/domain/src/repository/product.ts
@@ -1,12 +1,12 @@
 import type {IProduct, IProductEntity} from "../entity/product.js";
 
 export interface IProductRepository {
-    findAll(): Promise<IProduct[]>;
-    findById(id: number): Promise<IProduct | null>;
+    findAll(): Promise<IProductEntity>;
+    findById(id: number): Promise<IProductEntity>;
 
-    create(product: string): Promise<IProduct>;
+    create(product_name: IProduct): Promise<IProductEntity>;
 
-    update(code: string , product_name: string , cost: number): Promise<IProduct>;
+    update(code: string , product_name: Promise<IProductEntity> , cost: number): Promise<IProductEntity>;
 
-    delete(id: number): Promise<void>;
+    delete(id: number): Promise<boolean>;
 }
