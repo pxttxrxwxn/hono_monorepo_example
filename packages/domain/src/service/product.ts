059033bf054cb0c7b7d1e179b33cdce15891f5aa
pxttxrxwxn <naysasatadur5555@gmail.com>
2025-12-18 21:02:35 +0700

    update Assignment

--- packages/domain/src/service/product.ts
+++ packages/domain/src/service/product.ts
@@ -1,7 +1,10 @@
+//อธิบาย input ว่าเอาอะไรเข้ามา
 import type {ProductCreateDto, ProductDto} from "../dto/product.dto.js";
 
 export interface IProductService {
 
-    findAllUser() : Promise<ProductDto[]>
+    // ไม่รับข้อมูลแต่ให้ส่งผลลัพท์เป็น productdto
+    findAllProduct() : Promise<ProductDto[]>
+    // สร้าง product และส่งผลลัพท์เป็น productdto
     create(productDto : ProductCreateDto) : Promise<ProductDto>
 }
--- packages/domain/src/service/user.ts
+++ packages/domain/src/service/user.ts
@@ -2,6 +2,7 @@ import type {UserCreateDto, UserDto} from "../dto/user.dto.js";
 
 export interface IUserService {
 
+    //UserDtoค่าfontrendต้องการ
     findAllUser() : Promise<UserDto[]>
     create(userDto : UserCreateDto) : Promise<UserDto>
 }
